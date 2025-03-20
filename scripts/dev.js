856129e2631dec1c199d2335609f04119c102fac
Patryk Barć <patrykbarc.design@gmail.com>
2025-03-20 10:45:49 +0100

    Refactor development script to improve service readiness checks and logging.

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,40 +4,23 @@ import waitOn from 'wait-on'
 const logger = (message) => {
   console.log('\x1b[36m%s\x1b[0m', message)
 }
-// Function to check if all services are ready
-const checkAllReady = () => {
-  console.log('\n')
-  logger('✨ Development environment is ready! ✨')
-  logger('Server: http://localhost:3333')
-  logger('Client: http://localhost:5173')
-}
 
-const server = spawn('pnpm', ['run', 'backend'], {
+spawn('pnpm', ['run', 'backend'], {
   stdio: 'inherit',
   shell: true
 })
 
-const client = spawn('pnpm', ['run', 'frontend'], {
+spawn('pnpm', ['run', 'frontend'], {
   stdio: 'inherit',
   shell: true
 })
 
-// Wait for server to be ready
-waitOn({ resources: ['http://localhost:5173'] }).then(() => {
-  // console.error('Server running at http://localhost:3333')
-  // services.backend = true
-  checkAllReady()
-})
-
-// setTimeout(() => {
-//   const client = spawn('pnpm', ['run', 'frontend'], {
-//     stdio: 'inherit',
-//     shell: true
-//   })
-
-//   process.on('SIGINT', () => {
-//     server.kill()
-//     client.kill()
-//     process.exit()
-//   })
-// }, 1000)
+waitOn({ resources: ['http://localhost:5173'] })
+  .then(waitOn({ resources: ['http://localhost:3333'] }))
+  .then(() => {
+    console.log('\n')
+    logger('✨ Development environment is ready! ✨')
+    logger(' ➜  [Server]: http://localhost:3333')
+    logger(' ➜  [Client]: http://localhost:5173')
+    console.log('\n')
+  })
